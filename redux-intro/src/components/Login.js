04cc8880fb1fb71817bbe5a83081c0fb3c14752d
IdilYilmaz <idil.yilmaz@ug.bilkent.edu.tr>
2022-05-13 03:14:46 +0300

    css file name changed

--- redux-intro/src/components/Login.js
+++ redux-intro/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import "./csscomponents/LogReg.css";
+import "./csscomponents/components.css";
 import { useNavigate } from 'react-router-dom';
 import {
     BrowserRouter as Router,

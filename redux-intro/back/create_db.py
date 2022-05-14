5627ae88ae1d3d9953ba17b26b43ba3af19d43f1
turgut-edis <alp.edis@ug.bilkent.edu.tr>
2022-05-14 22:39:19 +0300

    Database fixing

--- redux-intro/back/create_db.py
+++ redux-intro/back/create_db.py
@@ -11,6 +11,14 @@ mydb = mysql.connector.connect(
 
 my_cursor = mydb.cursor()
 
+my_cursor.execute("DROP TABLE IF EXISTS buys, edits, shared_slip, like_comment, \
+                   plays, banned_editors, placed_on, banned_users, competitor_contest, \
+                   bet_slip_comment, match_comment, team, esports_team, competitors,\
+                   lol_results, football_results, basketball_results, volleyball_results,\
+                   results, item_coupon, comments, bet_slip, bet, matches, contest,\
+                   sports, editor_request, normal_user_follows, normal_user_friend,\
+                   editor, normal_user, slip_creator, admin, user")
+
 
 #student_Table = "CREATE TABLE student " + "(sid CHAR(12)," + "sname VARCHAR(50) NOT NULL," + "bdate DATE NOT NULL," + "address VARCHAR(50) NOT NULL," + "scity VARCHAR(20) NOT NULL," + "year CHAR(20) NOT NULL," + "gpa FLOAT NOT NULL," + "nationality VARCHAR(20) NOT NULL," + "PRIMARY KEY(sid))"
 
@@ -41,12 +49,8 @@ slip_creator(creator_ID) ON DELETE CASCADE \
 
 editor_request = "CREATE TABLE editor_request(\
 editor_ID INT, \
-admin_ID INT, \
 status VARCHAR(10), \
 PRIMARY KEY (editor_ID), \
-FOREIGN KEY (admin_ID) REFERENCES \
-admin(admin_ID)\
-ON DELETE CASCADE ON UPDATE CASCADE, \
 FOREIGN KEY (editor_ID) REFERENCES \
 editor(editor_ID)\
 ON DELETE CASCADE ON UPDATE CASCADE, \
@@ -68,7 +72,305 @@ FOREIGN KEY (admin_ID) REFERENCES \
 user(user_ID) ON DELETE CASCADE \
 )"
 
+bet = "CREATE TABLE bet( bet_ID INT, match_ID INT, mbn INT, ratio FLOAT (2,2), change_date TIMESTAMP, bet_type VARCHAR(30), active BOOLEAN, result VARCHAR(10), PRIMARY KEY (bet_ID, match_ID), FOREIGN KEY (match_ID) REFERENCES matches(match_ID) ON DELETE CASCADE ON UPDATE CASCADE, CHECK (result IN ('WON', 'RESULT', 'PENDING')) )"
 
+bet_slip = "CREATE TABLE bet_slip(\
+bet_slip_ID INT, \
+creator_ID INT, \
+bet_count INT, \
+total_amount INT, \
+isPlayed BOOLEAN, \
+PRIMARY KEY (bet_slip_ID), \
+FOREIGN KEY (creator_ID) REFERENCES slip_creator(creator_ID) \
+)"
+
+comments = "CREATE TABLE comments( \
+comment_ID INT, \
+user_ID INT, \
+comment VARCHAR(20000), \
+comment_date TIMESTAMP, \
+like_count INT, \
+PRIMARY KEY (comment_ID), \
+FOREIGN KEY(user_ID) REFERENCES \
+user(user_ID) ON DELETE CASCADE ON UPDATE \
+CASCADE \
+)"
+
+item_coupon = "CREATE TABLE item_coupon( \
+item_ID INT, \
+description VARCHAR(2000), \
+coupon_amount INT, \
+coupon_count INT, \
+sold_coupons INT, \
+PRIMARY KEY (item_ID) \
+)"
+
+matches = "CREATE TABLE matches( \
+match_ID INT, \
+start_date TIMESTAMP, \
+contest_ID INT, \
+season VARCHAR(20), \
+sport_name VARCHAR(15), \
+PRIMARY KEY(match_ID), \
+FOREIGN KEY(contest_ID, season, sport_name) \
+REFERENCES contest(contest_ID, sport_name, \
+season) ON DELETE CASCADE ON UPDATE \
+CASCADE, \
+FOREIGN KEY(sport_name) REFERENCES \
+sports(sport_name) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+)"
+
+results = "CREATE TABLE results( \
+result_ID INT, \
+match_ID INT, \
+home_score INT, \
+away_score INT, \
+PRIMARY KEY(result_ID), \
+FOREIGN KEY(match_ID) REFERENCES \
+matches(match_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+)"
+
+volleyball_results = "CREATE TABLE volleyball_results( \
+v_result_ID INT, \
+home_set_score INT, \
+away_set_score INT, \
+PRIMARY KEY(v_result_ID), \
+FOREIGN KEY (v_result_ID) REFERENCES \
+results(result_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+)"
+
+basketball_results = "CREATE TABLE basketball_results( \
+b_result_ID INT, \
+home_half_score INT, \
+away_half_score INT, \
+home_total_rebound_score INT, \
+away_total_rebound_score INT, \
+PRIMARY KEY(b_result_ID), \
+FOREIGN KEY (b_result_ID) REFERENCES \
+results(result_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+)"
+
+football_results = "CREATE TABLE football_results( \
+f_result_ID INT, \
+yellow_card_num INT, \
+red_card_num INT, \
+corner_cnt INT, \
+first_half_home_goals INT, \
+first_half_away_goals INT, \
+PRIMARY KEY(f_result_ID), \
+FOREIGN KEY (f_result_ID) REFERENCES \
+results(result_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+)"
+
+lol_results = "CREATE TABLE lol_results(\
+l_result_ID INT, \
+winner_side VARCHAR(4), \
+elder_dragon_side VARCHAR(4), \
+baron_side VARCHAR(4), \
+soul_side VARCHAR(4), \
+first_tower_side VARCHAR(4), \
+first_blood_side VARCHAR(4), \
+PRIMARY KEY(l_result_ID), \
+FOREIGN KEY (l_result_ID) REFERENCES \
+results(result_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE, \
+CHECK(winner_side IN ('HOME', 'AWAY')), \
+CHECK(elder_dragon_side IN ('HOME', 'AWAY')), \
+CHECK(baron_side IN ('HOME', 'AWAY')), \
+CHECK(soul_side IN ('HOME', 'AWAY')), \
+CHECK(first_tower_side IN ('HOME', 'AWAY')), \
+CHECK(first_blood_side IN ('HOME', 'AWAY')) \
+)"
+
+esports_team = "CREATE TABLE esports_team( \
+competitor_ID INT, \
+PRIMARY KEY(competitor_ID), \
+FOREIGN KEY (competitor_ID) REFERENCES \
+competitors(competitor_ID) ON DELETE CASCADE \
+ON UPDATE CASCADE \
+)" 
+
+
+team = "CREATE TABLE team( \
+competitor_ID INT, \
+name VARCHAR(50) NOT NULL, \
+location VARCHAR(200) NOT NULL, \
+PRIMARY KEY(competitor_ID), \
+FOREIGN KEY (competitor_ID) REFERENCES \
+competitors(competitor_ID) ON DELETE CASCADE \
+ON UPDATE CASCADE \
+)"
+
+sports = "CREATE TABLE sports( \
+sport_name VARCHAR(15), \
+PRIMARY KEY(sport_name) \
+)"
+
+contest = "CREATE TABLE contest( \
+contest_ID INT, \
+sport_name VARCHAR(15), \
+name VARCHAR(30), \
+season VARCHAR(20), \
+PRIMARY KEY (contest_ID, sport_name, season), \
+FOREIGN KEY (sport_name) REFERENCES \
+sports(sport_name) ON DELETE CASCADE ON UPDATE CASCADE \
+)"
+
+competitors = "CREATE TABLE competitors( \
+competitor_ID INT, \
+team_name VARCHAR(50), \
+win_rate FLOAT, \
+colors VARCHAR(30), \
+coach_name VARCHAR(2000), \
+estab_date INT, \
+PRIMARY KEY(competitor_ID) \
+)"
+
+match_comment = "CREATE TABLE match_comment( \
+match_ID INT, \
+comment_ID INT, \
+PRIMARY KEY(match_ID, comment_ID), \
+FOREIGN KEY(match_ID) REFERENCES \
+matches(match_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE, \
+FOREIGN KEY(comment_ID) REFERENCES \
+comments(comment_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+)"
+
+bet_slip_comment = "CREATE TABLE bet_slip_comment( \
+comment_ID INT, \
+bet_slip_ID INT, \
+PRIMARY KEY(comment_ID, bet_slip_ID), \
+FOREIGN KEY(comment_ID) REFERENCES \
+comments(comment_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE, \
+FOREIGN KEY(bet_slip_ID) REFERENCES \
+bet_slip(bet_slip_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+)"
+
+competitor_contest = "CREATE TABLE competitor_contest( \
+competitor_ID INT, \
+contest_ID INT, \
+sport_name VARCHAR(15), \
+season VARCHAR(20), \
+points INT, \
+PRIMARY KEY(competitor_ID, contest_ID, season), \
+FOREIGN KEY (competitor_ID) REFERENCES \
+competitors(competitor_ID) ON DELETE CASCADE \
+ON UPDATE CASCADE, \
+FOREIGN KEY(contest_ID, sport_name, season) REFERENCES \
+contest(contest_ID, sport_name, season) ON DELETE CASCADE \
+ON UPDATE CASCADE \
+)"
+
+banned_users = "CREATE TABLE banned_users( \
+admin_ID INT, \
+n_user_ID INT, \
+PRIMARY KEY(admin_ID, n_user_ID), \
+FOREIGN KEY(admin_ID) REFERENCES \
+admin(admin_ID) ON DELETE CASCADE, \
+FOREIGN KEY(n_user_ID) REFERENCES \
+normal_user(n_user_ID) ON DELETE CASCADE \
+)"
+
+placed_on = "CREATE TABLE placed_on( \
+bet_slip_ID INT, \
+bet_ID INT, \
+match_ID INT, \
+PRIMARY KEY(bet_slip_ID, match_ID), \
+FOREIGN KEY(bet_slip_ID) REFERENCES \
+bet_slip(bet_slip_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE, \
+FOREIGN KEY(bet_ID, match_ID) REFERENCES \
+bet(bet_ID, match_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE, \
+FOREIGN KEY(match_ID) REFERENCES \
+matches(match_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+)"
+
+banned_editors = "CREATE TABLE banned_editors( \
+admin_ID INT, \
+editor_ID INT, \
+PRIMARY KEY(admin_ID, editor_ID), \
+FOREIGN KEY(admin_ID) REFERENCES \
+admin(admin_ID) ON DELETE CASCADE, \
+FOREIGN KEY(editor_ID) REFERENCES \
+editor(editor_ID) ON DELETE CASCADE \
+)"
+
+
+plays = "CREATE TABLE plays( \
+match_ID INT, \
+competitor_ID INT, \
+side VARCHAR(4), \
+PRIMARY KEY(match_ID, competitor_ID), \
+FOREIGN KEY(match_ID) REFERENCES \
+matches(match_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE, \
+FOREIGN KEY (competitor_ID) REFERENCES \
+competitors(competitor_ID) ON DELETE CASCADE \
+ON UPDATE CASCADE, \
+CHECK ( side IN ( 'HOME', 'AWAY' ) ) \
+) \
+"
+
+like_comment = "CREATE TABLE like_comment( \
+comment_ID INT, \
+n_user_ID INT, \
+PRIMARY KEY(comment_ID, n_user_ID), \
+FOREIGN KEY(n_user_ID) REFERENCES \
+normal_user(n_user_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE, \
+FOREIGN KEY (comment_ID) REFERENCES \
+comments(comment_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+)" \
+
+shared_slip = "CREATE TABLE shared_slip( \
+bet_slip_ID INT, \
+sharer_ID INT, \
+PRIMARY KEY(bet_slip_ID, sharer_ID), \
+FOREIGN KEY(bet_slip_ID) REFERENCES \
+bet_slip(bet_slip_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE, \
+FOREIGN KEY (sharer_ID) REFERENCES \
+slip_creator(creator_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+)"
+
+edits = "CREATE TABLE edits( \
+admin_ID INT, \
+bet_ID INT, \
+match_ID INT, \
+PRIMARY KEY(admin_ID, bet_ID, match_ID), \
+FOREIGN KEY (admin_ID) REFERENCES \
+admin(admin_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE, \
+FOREIGN KEY(bet_ID, match_ID) REFERENCES \
+bet(bet_ID, match_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+) \
+"
+
+buys = "CREATE TABLE buys( \
+item_ID INT, \
+n_user_ID INT, \
+PRIMARY KEY(item_ID, n_user_ID), FOREIGN \
+KEY(item_ID) REFERENCES item_coupon(item_ID) \
+ON DELETE CASCADE ON UPDATE CASCADE, \
+FOREIGN KEY(n_user_ID) REFERENCES \
+normal_user(n_user_ID) ON DELETE CASCADE ON \
+UPDATE CASCADE \
+) \
+"
 
 my_cursor.execute(user)
 my_cursor.execute(admin)
@@ -78,7 +380,39 @@ my_cursor.execute(editor)
 my_cursor.execute(normal_user_friend)
 my_cursor.execute(normal_user_follows)
 my_cursor.execute(editor_request)
+my_cursor.execute(sports)
+my_cursor.execute(contest)
+my_cursor.execute(matches)
+my_cursor.execute(bet)
+my_cursor.execute(bet_slip)
+my_cursor.execute(comments)
+my_cursor.execute(item_coupon)
+my_cursor.execute(results)
+my_cursor.execute(volleyball_results)
+my_cursor.execute(basketball_results)
+my_cursor.execute(football_results)
+my_cursor.execute(lol_results)
+my_cursor.execute(competitors)
+my_cursor.execute(esports_team)
+my_cursor.execute(team)
+my_cursor.execute(match_comment)
+my_cursor.execute(bet_slip_comment)
+my_cursor.execute(competitor_contest)
+my_cursor.execute(banned_users)
+my_cursor.execute(placed_on)
+my_cursor.execute(banned_editors)
+my_cursor.execute(plays)
+my_cursor.execute(like_comment)
+my_cursor.execute(shared_slip)
+my_cursor.execute(edits)
+my_cursor.execute(buys)
 
-
+#my_cursor.execute("DROP TABLE IF EXISTS buys, edits, shared_slip, like_comment, \
+#                   plays, banned_editors, placed_on, banned_users, competitor_contest, \
+#                   bet_slip_comment, match_comment, team, esports_team, competitors,\
+#                   lol_results, football_results, basketball_results, volleyball_results,\
+#                   results, item_coupon, comments, bet_slip, bet, matches, contest,\
+#                   sports, editor_request, normal_user_follows, normal_user_friend,\
+#                   editor, normal_user, slip_creator, admin, user")
 
 
--- redux-intro/back/hello.py
+++ redux-intro/back/hello.py
@@ -1,16 +1,141 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+import json
+import time
+from flask import Flask, render_template, request, flash, session, jsonify, redirect
+from flask_cors import CORS
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE__URI'] = 'mysql+pymysql://root:password123@localhost/our_users'
 
-@app.route('/')
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_DB'] = 'our_users'
+app.config['MYSQL_PASSWORD'] = 'password123'
+app.config['MYSQL_USER'] = 'root'
 
-def index():
-    return "<h1>Hello World!</h1>"
+sql = MySQL(app)
+CORS(app)
 
-@app.route('/user/<name>')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    cursor = sql.connection.cursor()
+ 
+    return "<h1>HOME PAGE</h1>"
 
-def user(name):
-    return "<h1>Hello {}</h1>".format(name)
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        userInfo = request.get_json()
+        if userInfo.get('pass') != userInfo.get('confirm_pass'):
+            res = { "result" : "false" }
+            return jsonify({"result": res})
+        cursor = sql.connection.cursor()
+        passw = userInfo.get('confirm_pass')
+
+        cursor.execute("INSERT INTO user(username, password, name, surname, birth_year, mail) " 
+                    "VALUES ( %s, %s, %s, %s, %s, %s ) ", (userInfo.get('username'), passw, userInfo.get("name"), userInfo.get('surname'), userInfo.get("birth_year"), userInfo.get("mail")))
+        
+        #
+
+        sql.connection.commit()
+
+        
+        cursor.execute( "SELECT user_ID from user WHERE username = %s", ([userInfo['username']]) )
+        user = cursor.fetchone()
+        ID = user[0]
+
+        if userInfo.get('type') == "user":
+            insert_user = "INSERT INTO slip_creator(creator_ID) VALUES ({0})".format(ID)
+            cursor.execute(insert_user)
+            sql.connection.commit()
+
+            creator = cursor.fetchone()
+            creator_ID = ID
+            val = (creator_ID, 0, 0, False)
+            cursor.execute("INSERT INTO bet_slip(creator_ID, bet_count, total_amount, isPlayed) VALUES (%s, %s, %s, %s)", (val))   
+            
+            sql.connection.commit()
+            
+            val = (ID, 0, 0, userInfo['address'], 0)
+            cursor.execute("INSERT INTO normal_user(n_user_ID, balance, winning_cnt, address, coupons) VALUES ( %s, %s, %s, %s, %s)", (val))
+            sql.connection.commit()
+            
+        if userInfo.get('type') == "editor":
+            insert_editor = "INSERT INTO editor_request(editor_ID, status) VALUES({0}, {1})".format(ID, "PENDING")
+            cursor.execute(insert_editor)
+            sql.connection.commit()
+
+        result = {
+            "success": "true",
+            "type": userInfo.get('type'),
+            "user_ID": ID,
+            "username": userInfo.get('username')
+        }
+        return jsonify({"result": result})
+
+@app.route('/signin', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        userInfo = request.get_json()
+        cursor = sql.connection.cursor()
+        value = cursor.execute("SELECT * FROM user WHERE username = %s", ([userInfo['username']]))
+        if value > 0:
+            user = cursor.fetchone()
+            if user[2] == userInfo['password']:
+                normal_user = cursor.execute("SELECT * FROM normal_user WHERE n_user_id = %s", ([user[0]]))
+                normal_users = cursor.fetchone()
+                if normal_user > 0:
+                    ban = cursor.execute("SELECT * FROM banned_users WHERE n_user_id = %s", ([normal_users[0]]))
+                    if ban > 0:
+                        result = {
+                            "success" : "false",
+                            "ban" : "true"
+                        }
+                    else:
+                        type = "user"
+                        result = {
+                            "success" : "true",
+                            "type" : type,
+                            "user_id" : normal_users[0],
+                            "username" : userInfo['username'],
+                            "balance" : normal_users[1]
+                        }    
+                else:
+                    editor = cursor.execute("SELECT editor_ID FROM editor WHERE editor_ID = %s", ([user[0]]))
+                    if editor > 0:
+                        editorBan = cursor.execute("SELECT * FROM banned_editors WHERE editor_ID = %s", ([normal_users[0]]))
+                        if editorBan > 0:
+                            result = {
+                                "success" : "false",
+                                "ban" : "true"
+                            }
+                        else:
+                            type = "editor"
+                            result = {
+                                "success" : "true",
+                                "type" : type,
+                                "editor_ID" : user[0],
+                                "username" : userInfo['username']
+                         }
+                    else:
+                        admin = cur.execute("SELECT admin_ID FROM admin WHERE admin_ID = %s", ([user[0]]))
+                        if admin > 0:
+                            type = "admin"
+                            result = {
+                                "success" : "true",
+                                "type" : type,
+                                "user_id" : user[0],
+                                "username" : userInfo['username']
+                            }
+                        else:
+                            result = {
+                                "success" : "false",
+                                "pending" : "true"
+                            }
+        else:
+            result = {
+                "success" : "false",
+                "ban" : "false"
+            }         
+        return jsonify({"result" : result})
+
+app.run(debug=True)

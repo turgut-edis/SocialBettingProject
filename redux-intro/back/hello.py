30c5fdab7e1fa5186c62b55ddc68baa3bd813102
turgut-edis <alp.edis@ug.bilkent.edu.tr>
2022-05-14 11:18:53 +0300

    Initialization of Backend

---
+++ redux-intro/back/hello.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
+if mysql.connection is not None: 
+    #Creating a connection cursor
+    cursor = mysql.connection.cursor()
+    
+    #Executing SQL Statements
+    cursor.execute(''' CREATE TABLE table_name(field1, field2...) ''')
+    cursor.execute(''' INSERT INTO table_name VALUES(v1,v2...) ''')
+    cursor.execute(''' DELETE FROM table_name WHERE condition ''')
+    
+    #Saving the Actions performed on the DB
+    mysql.connection.commit()
+    
+    #Closing the cursor
+    cursor.close()
+ 
+
+ 
+#Closing the cursor
+
